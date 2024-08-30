93ca4ea2587d4958044eca94148c558ed277e991
Abari Dodo <dodo.mamane.abari@gmail.com>
2024-08-30 15:16:13 +0200

    feat(background-image): set a mario wall background image

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,17 @@ from utils import chat
 app = Flask(__name__)
 conversation = [
 ]
-
-
+prompt = """You are AbariBot, an assistant the response 
+            Only questions related to Abari. You are linked to 
+            Vectors database That contain information about Abari.
+            Don't respond to question not related to Abari; user question: """
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global conversation
     if request.method == 'POST':
         user_message = request.form['user_message']
-        bot_response = chat.invoke({"question": user_message})['answer']
+        question = prompt + user_message
+        bot_response = chat.invoke({"question": question})['answer']
         conversation.append((user_message, bot_response))
         return redirect(url_for('index'))
     return render_template('abariBot.html', conversation=conversation)
